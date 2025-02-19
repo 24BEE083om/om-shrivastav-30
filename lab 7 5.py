090e06516ab97fdf75d504493c6abd680b566da8
24BEE083om <24BEE083@spt.pdpu.ac.in>
2025-02-19 12:33:16 +0530

    Add files via upload

---
+++ lab 7 5.py
@@ -0,0 +1,20 @@
+print("om shrivastav")
+print("24BEE083")
+grocery_prices = {
+    "apples": 3.00,   # Price per kg
+    "bananas": 1.50,  # Price per dozen
+    "bread": 2.00,    # Price per loaf
+    "milk": 1.20,     # Price per liter
+    "eggs": 2.50      # Price per dozen
+}
+
+grocery_quantities = {
+    "apples": 2,   # Quantity in kg
+    "bananas": 1,  # Quantity in dozen
+    "bread": 3,    # Quantity in loaves
+    "milk": 2,     # Quantity in liters
+    "eggs": 1      # Quantity in dozen
+}
+
+print("Grocery Prices:", grocery_prices)
+print("Grocery Quantities:", grocery_quantities)
